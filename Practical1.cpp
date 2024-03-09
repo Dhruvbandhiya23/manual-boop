1c727457d675fe69f05938eb30114a0e36529899
Dhruvbandhiya23 <161951566+Dhruvbandhiya23@users.noreply.github.com>
2024-03-09 08:30:52 +0530

    p 1

---
+++ Practical1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+double a;
+double b;
+double add;
+double sub;
+double multi;
+double div;
+cout<<" enter any two numbers";
+cin >> a >>b;
+add=a+b;
+sub=a-b;
+multi=a*b;
+div=a/b;
+
+cout<<"the addition of two numbers is : "<<add<<endl;
+cout<<"the substraction of two numbers is : "<<sub<<endl;
+cout<<"the multiplication of two numbers is : "<<multi<<endl;
+cout<<"the divison of two numbers is : "<<div<<endl;
+
+
+  
+
+
+
+return 0;
+}
