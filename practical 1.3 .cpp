b40dc943f7e3847848dbd919db4f865c65d44014
Dhruvbandhiya23 <161951566+Dhruvbandhiya23@users.noreply.github.com>
2024-03-09 09:09:43 +0530

    que:3

---
+++ practical 1.3 .cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+
+int i;
+cout<<"enter a value of i";
+cin>>i;
+if(i%2==0){
+cout<<"The number is a prime number";
+
+
+}
+else{
+
+
+    cout<<"The number is not prime number  ";
+
+}
+
+return 0;
+}
