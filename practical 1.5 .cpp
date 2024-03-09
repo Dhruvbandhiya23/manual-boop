f4da5539c30a9691964736ee13c7b4a7cdb2f501
Dhruvbandhiya23 <161951566+Dhruvbandhiya23@users.noreply.github.com>
2024-03-09 09:13:44 +0530

    que :5

---
+++ practical 1.5 .cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+int sum=100;
+int main()
+{
+int sum=500;
+cout<<"the local value of sum is="<<sum<<endl;
+cout<<"the global value of sum is="<<::sum<<endl;
+
+
+
+return 0;
+}
