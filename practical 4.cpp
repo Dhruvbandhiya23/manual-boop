736b16b21576a5780bdb99180e95ff5cf7319ee3
Dhruvbandhiya23 <161951566+Dhruvbandhiya23@users.noreply.github.com>
2024-03-21 15:12:35 +0530

    Practical 4 code 2

---
+++ practical 4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+double multi(double a,double b){
+
+return a*b;
+
+
+};
+int main()
+{
+int x,y;
+    cout << "enter two no." << endl;
+    cin >> x>>y;
+
+    cout << "the multiplication of two no. is " <<multi (x,y)<< endl;
+    
+    
+    return 0;
+}
